4b9edab3d560f5549120fa08651566b34d65db81
giuvallente <giuliagv@al.insper.edu.br>
2025-05-18 12:07:52 -0300

    tela vitoria

---
+++ Assets/Scripts/Jogo/Reiniciar.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Reiniciar : MonoBehaviour
+{
+    public void Menu()
+   {
+       // Carrega a cena do menu
+       UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+   }
+}
